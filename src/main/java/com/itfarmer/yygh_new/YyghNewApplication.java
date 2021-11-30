4ec6f467670568868dad87666c3283fd427bc8f8
Mr.Li <liyangyang08144@foxmail.com>
2021-12-01 00:59:17 +0800

    Initial commit

---
+++ src/main/java/com/itfarmer/yygh_new/YyghNewApplication.java
@@ -0,0 +1,13 @@
+package com.itfarmer.yygh_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyghNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyghNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itfarmer/yygh_new/YyghNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itfarmer.yygh_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyghNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
